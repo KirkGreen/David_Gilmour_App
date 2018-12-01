ec970e45c3e5005b2365cfd842ea1a6e644fda3c
Kirill <KirillGrinev@gmail.com>
2018-12-01 17:41:52 +0200

    styling side navigation

--- src/components/Header/SideNav/sideNav_item.js
+++ src/components/Header/SideNav/sideNav_item.js
@@ -2,43 +2,37 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import FontAwesome from 'react-fontawesome';
-import style from './sideNav.css'
+import './sideNav.css'
 
 const SideNavItem = () => {
 
     const item = [
         {
-            type: style.option,
             icon: 'home',
-            text: 'home',
+            text: 'Home',
             link: '/'
         },
         {
-            type: style.option,
             icon: 'file-text-o',
             text: 'News',
             link: '/news'
         },
         {
-            type: style.option,
-            icon: 'file-audio-o',
+            icon: 'folder-o',
             text: 'Discography',
             link: '/Discography'
         },
         {
-            type: style.option,
             icon: 'play',
             text: 'Videos',
             link: '/videos'
         },
         {
-            type: style.option,
             icon: 'sign-in',
             text: 'Sign in',
             link: '/sign-in'
         },
         {
-            type: style.option,
             icon: 'sign-out',
             text: 'Sign out',
             link: '/sign-out'
@@ -47,9 +41,9 @@ const SideNavItem = () => {
     ];
 
     const showItem = () => {
-        return item.map((item, i) => {
+        return item.map( (item, i) => {
             return (
-                <div key={i} className={item.type}>
+                <div key={i} className="option">
                     <Link to={item.link}>
                         <FontAwesome name={item.icon}/>
                         {item.text}
